90bd1a8ee1562f8c1e1b478cbe6520febd351242
Lise Kyle Chapman <lisekyle@gmail.com>
2022-02-06 15:16:08 -0800

    Create home_scripts.js

---
+++ home_scripts.js
@@ -0,0 +1,371 @@
+
+const tl = gsap.timeline();
+
+	tl.from(".line div", 1.2, {
+    y: -25,
+    ease: "power4.out",
+    delay: 1,
+    skewY: 5,
+    opacity: 0,
+    stagger: {
+      amount: 0.3
+    }
+	})
+
+{
+    // body element
+    const body = document.body;
+
+    // helper functions
+    const MathUtils = {
+        // linear interpolation
+        lerp: (a, b, n) => (1 - n) * a + n * b,
+        // distance between two points
+        distance: (x1,y1,x2,y2) => Math.hypot(x2-x1, y2-y1)
+    }
+
+    // get the mouse position
+    const getMousePos = (ev) => {
+        let posx = 0;
+        let posy = 0;
+        if (!ev) ev = window.event;
+        if (ev.pageX || ev.pageY) {
+            posx = ev.pageX;
+            posy = ev.pageY;
+        }
+        else if (ev.clientX || ev.clientY) 	{
+            posx = ev.clientX + body.scrollLeft + docEl.scrollLeft;
+            posy = ev.clientY + body.scrollTop + docEl.scrollTop;
+        }
+        return {x: posx, y: posy};
+    }
+
+    // mousePos: current mouse position
+    // cacheMousePos: previous mouse position
+    // lastMousePos: last last recorded mouse position (at the time the last image was shown)
+    let mousePos = lastMousePos = cacheMousePos = {x: 0, y: 0};
+    
+    // update the mouse position
+    window.addEventListener('mousemove', ev => mousePos = getMousePos(ev));
+    
+    // gets the distance from the current mouse position to the last recorded mouse position
+    const getMouseDistance = () => MathUtils.distance(mousePos.x,mousePos.y,lastMousePos.x,lastMousePos.y);
+
+    class Image {
+        constructor(el) {
+            this.DOM = {el: el};
+            // image deafult styles
+            this.defaultStyle = {
+                scale: 1,
+                x: 0,
+                y: 0,
+                opacity: 0
+            };
+            // get sizes/position
+            this.getRect();
+            // init/bind events
+            this.initEvents();
+        }
+        initEvents() {
+            // on resize get updated sizes/position
+            window.addEventListener('resize', () => this.resize());
+        }
+        resize() {
+            // reset styles
+            gsap.set(this.DOM.el, this.defaultStyle);
+            // get sizes/position
+            this.getRect();
+        }
+        getRect() {
+            this.rect = this.DOM.el.getBoundingClientRect();
+        }
+        isActive() {
+            // check if image is animating or if it's visible
+            return gsap.isTweening(this.DOM.el) || this.DOM.el.style.opacity != 0;
+        }
+    }
+
+    class ImageTrail {
+        constructor() {
+            // images container
+            this.DOM = {content: document.querySelector('.content')};
+            // array of Image objs, one per image element
+            this.images = [];
+            [...this.DOM.content.querySelectorAll('img')].forEach(img => this.images.push(new Image(img)));
+            // total number of images
+            this.imagesTotal = this.images.length;
+            // upcoming image index
+            this.imgPosition = 0;
+            // zIndex value to apply to the upcoming image
+            this.zIndexVal = 1;
+            // mouse distance required to show the next image
+            this.threshold = 100;
+            // Give the scroll handler access to this context
+            this.handleScroll = this.handleScroll.bind(this);
+            // render the images
+            requestAnimationFrame(() => this.render());
+        }
+        handleScroll() {
+          if ( document.scrollingElement.scrollTop === 0 ) {
+            document.removeEventListener('scroll', this.handleScroll);
+            requestAnimationFrame(() => this.render());
+          }
+        }
+        render() {
+        		// Only activate when scrolled to the top
+            if ( document.scrollingElement.scrollTop > 200 ) {
+            	document.addEventListener('scroll', this.handleScroll)
+              return;
+            }
+        	
+            // get distance between the current mouse position and the position of the previous image
+            let distance = getMouseDistance();
+            // cache previous mouse position
+            cacheMousePos.x = MathUtils.lerp(cacheMousePos.x || mousePos.x, mousePos.x, 0.1);
+            cacheMousePos.y = MathUtils.lerp(cacheMousePos.y || mousePos.y, mousePos.y, 0.1);
+
+            // if the mouse moved more than [this.threshold] then show the next image
+            if ( distance > this.threshold ) {
+                this.showNextImage();
+
+                ++this.zIndexVal;
+                this.imgPosition = this.imgPosition < this.imagesTotal-1 ? this.imgPosition+1 : 0;
+                
+                lastMousePos = mousePos;
+            }
+
+            // check when mousemove stops and all images are inactive (not visible and not animating)
+            let isIdle = true;
+            for (let img of this.images) {
+                if ( img.isActive() ) {
+                    isIdle = false;
+                    break;
+                }
+            }
+            // reset z-index initial value
+            if ( isIdle && this.zIndexVal !== 1 ) {
+                this.zIndexVal = 1;
+            }
+
+            // loop..
+            requestAnimationFrame(() => this.render());
+        }
+        showNextImage() {
+            // show image at position [this.imgPosition]
+            const img = this.images[this.imgPosition];
+            // kill any tween on the image
+            gsap.killTweensOf(img.DOM.el);
+
+          const tl = gsap.timeline();
+            // show the image
+            tl.set(img.DOM.el, {
+                startAt: {opacity: 0, scale: 1},
+                opacity: 1,
+                scale: 1,
+                zIndex: this.zIndexVal,
+                x: cacheMousePos.x - img.rect.width/2,
+                y: cacheMousePos.y - img.rect.height/2
+            }, 0)
+            // animate position
+            tl.to(img.DOM.el, 1.9, {
+                ease: Expo.easeOut,
+                x: mousePos.x - img.rect.width/2,
+                y: mousePos.y - img.rect.height/2
+            }, 0)
+            // then make it disappear
+            tl.to(img.DOM.el, 1, {
+                ease: Power1.easeOut,
+                opacity: 0
+            }, 1.4)
+            // scale down the image
+            tl.to(img.DOM.el, 1, {
+                ease: Quint.easeOut,
+                scale: 0.2
+            }, 1.4);
+        }
+    }
+
+
+    /***********************************/
+    /********** Preload stuff **********/
+
+    // Preload images
+    const preloadImages = () => {
+        return new Promise((resolve, reject) => {
+            imagesLoaded(document.querySelectorAll('.content__img'), resolve);
+        });
+    };
+    
+    // And then..
+    preloadImages().then(() => {
+        // Remove the loader
+        document.body.classList.remove('loading');
+        new ImageTrail();
+    });
+}
+
+
+window.onload = function () {
+document.getElementById("loadingIndicator").style.opacity = "0";
+setTimeout(function() {
+    document.getElementById("loadingIndicator").style.display = "none";}, 450);
+}
+
+{
+    const mapNumber = (X,A,B,C,D) => (X-A)*(D-C)/(B-A)+C;
+    // from http://www.quirksmode.org/js/events_properties.html#position
+	const getMousePos = (e) => {
+        let posx = 0;
+        let posy = 0;
+		if (!e) e = window.event;
+		if (e.pageX || e.pageY) {
+            posx = e.pageX;
+			posy = e.pageY;
+		}
+		else if (e.clientX || e.clientY) 	{
+			posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+			posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+		}
+        return { x : posx, y : posy }
+    }
+    // Generate a random float.
+    const getRandomFloat = (min, max) => (Math.random() * (max - min) + min).toFixed(2);
+
+    /**
+     * One class per effect. 
+     * Lots of code is repeated, so that single effects can be easily used. 
+     */
+
+    // Effect 1
+    class HoverImgFx1 {
+        constructor(el) {
+            this.DOM = {el: el};
+            this.DOM.reveal = document.createElement('div');
+            this.DOM.reveal.className = 'hover-reveal';
+            this.DOM.reveal.innerHTML = `<div class="hover-reveal__inner"><div class="hover-reveal__img" style="background-image:url(${this.DOM.el.dataset.img})"></div></div>`;
+            this.DOM.el.appendChild(this.DOM.reveal);
+            this.DOM.revealInner = this.DOM.reveal.querySelector('.hover-reveal__inner');
+            this.DOM.revealInner.style.overflow = 'hidden';
+            this.DOM.revealImg = this.DOM.revealInner.querySelector('.hover-reveal__img');
+
+            this.initEvents();
+        }
+        initEvents() {
+            this.positionElement = (ev) => {
+                const mousePos = getMousePos(ev);
+                const docScrolls = {
+                    left : document.body.scrollLeft + document.documentElement.scrollLeft, 
+                    top : document.body.scrollTop + document.documentElement.scrollTop
+                };
+                this.DOM.reveal.style.top = `${mousePos.y-20-docScrolls.top}px`;
+                this.DOM.reveal.style.left = `${mousePos.x-20-docScrolls.left}px`;
+            };
+            this.mouseenterFn = (ev) => {
+                this.positionElement(ev);
+                this.showImage();
+            };
+            this.mousemoveFn = ev => requestAnimationFrame(() => {
+                this.positionElement(ev);
+            });
+            this.mouseleaveFn = () => {
+                this.hideImage();
+            };
+            
+            this.DOM.el.addEventListener('mouseenter', this.mouseenterFn);
+            this.DOM.el.addEventListener('mousemove', this.mousemoveFn);
+            this.DOM.el.addEventListener('mouseleave', this.mouseleaveFn);
+        }
+         showImage() {
+            TweenMax.killTweensOf(this.DOM.revealInner);
+            TweenMax.killTweensOf(this.DOM.revealImg);
+
+            this.tl = new TimelineMax({
+                onStart: () => {
+                    this.DOM.reveal.style.opacity = 1;
+                    TweenMax.set(this.DOM.el, {zIndex: 1000});
+                }
+            })
+            .add('begin')
+            .add(new TweenMax(this.DOM.revealInner, 0.2, {
+                ease: Sine.easeOut,
+                startAt: {x: '-100%'},
+                x: '0%'
+            }), 'begin')
+            .add(new TweenMax(this.DOM.revealImg, 0.2, {
+                ease: Sine.easeOut,
+                startAt: {x: '100%'},
+                x: '0%'
+            }), 'begin');
+        }
+        hideImage() {
+            TweenMax.killTweensOf(this.DOM.revealInner);
+            TweenMax.killTweensOf(this.DOM.revealImg);
+
+            this.tl = new TimelineMax({
+                onStart: () => {
+                    TweenMax.set(this.DOM.el, {zIndex: 999});
+                },
+                onComplete: () => {
+                    TweenMax.set(this.DOM.el, {zIndex: ''});
+                    TweenMax.set(this.DOM.reveal, {opacity: 0});
+                }
+            })
+            .add('begin')
+            .add(new TweenMax(this.DOM.revealInner, 0.2, {
+                ease: Sine.easeOut,
+                x: '100%'
+            }), 'begin')
+            
+            .add(new TweenMax(this.DOM.revealImg, 0.2, {
+                ease: Sine.easeOut,
+                x: '-100%'
+            }), 'begin');
+        } showImage() {
+            TweenMax.killTweensOf(this.DOM.revealInner);
+            TweenMax.killTweensOf(this.DOM.revealImg);
+
+            this.tl = new TimelineMax({
+                onStart: () => {
+                    this.DOM.reveal.style.opacity = 1;
+                    TweenMax.set(this.DOM.el, {zIndex: 1000});
+                }
+            })
+            .add('begin')
+            .add(new TweenMax(this.DOM.revealInner, 0.2, {
+                ease: Sine.easeOut,
+                startAt: {x: '-100%'},
+                x: '0%'
+            }), 'begin')
+            .add(new TweenMax(this.DOM.revealImg, 0.2, {
+                ease: Sine.easeOut,
+                startAt: {x: '100%'},
+                x: '0%'
+            }), 'begin');
+        }
+        hideImage() {
+            TweenMax.killTweensOf(this.DOM.revealInner);
+            TweenMax.killTweensOf(this.DOM.revealImg);
+
+            this.tl = new TimelineMax({
+                onStart: () => {
+                    TweenMax.set(this.DOM.el, {zIndex: 999});
+                },
+                onComplete: () => {
+                    TweenMax.set(this.DOM.el, {zIndex: ''});
+                    TweenMax.set(this.DOM.reveal, {opacity: 0});
+                }
+            })
+            .add('begin')
+            .add(new TweenMax(this.DOM.revealInner, 0.2, {
+                ease: Sine.easeOut,
+                x: '100%'
+            }), 'begin')
+            
+            .add(new TweenMax(this.DOM.revealImg, 0.2, {
+                ease: Sine.easeOut,
+                x: '-100%'
+            }), 'begin');
+        }
+    }
+    [...document.querySelectorAll('.linkie')].forEach(link => new HoverImgFx1(link));
+   }
